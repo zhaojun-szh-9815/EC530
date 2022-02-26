cd9a4aa4132b4a484fa58ed5db77af106f8e879b
朝菌君桾覠頵 <55321300+zhaojun-szh-9815@users.noreply.github.com>
2022-02-25 21:07:53 -0500

    Update test_device_module.py
    update because device_module updated

--- Project2/test_device_module.py
+++ Project2/test_device_module.py
@@ -1,30 +1,44 @@
 import pytest
-import device_module as dev
+import device_module as dm
 
-@pytest.mark.parametrize('type, value_type, output',
-                         [('height_machine', 'Height',
-                           "[{'d_id': 1, 'type': 'sphygmomanometer', 'value_type': 'Blood_Pressure'}, {'d_id': 2, 'type': 'thermometer', 'value_type': 'Temperature'}, {'d_id': 3, 'type': 'pulse_device', 'value_type': 'Pulse'}, {'d_id': 4, 'type': 'oximeter', 'value_type': 'Blood_Oxygen'}, {'d_id': 5, 'type': 'weight_machine', 'value_type': 'Weight'}, {'d_id': 6, 'type': 'glucometer', 'value_type': 'Blood_Glucose'}, {'d_id': 7, 'type': 'height_machine', 'value_type': 'Height'}]")])
-def test_devices_registration(type, value_type, output):
-    dev.make_json_input()
-    devices = dev.device_registration(type, value_type)
-    assert str(devices) == output
+db_dir = './db_ec530_p2.db'
 
+@pytest.mark.parametrize('id, output',
+                         [('1', "(1, 'ZH', 'S', 'Male', 'Developer', '0123456789', '1998-01-05', 175, 80)"),
+                          ('2', "(2, 'AA', 'A', 'Female', 'Doctor', '1234567890', '1997-04-02', 170, 50)")])
+def test_select_user(id, output):
+    conn = dm.create_connection(db_dir)
+    user = dm.select_user(conn, id)
+    conn.close()
+    assert str(user) == output
 
-@pytest.mark.parametrize('assign_person, assigned_to, device, output',
-                         [(2, 3, 3,"[{'a_id': 1, 'assign_person': 2, 'assigned_to': 3, 'device': 1}, {'a_id': 2, 'assign_person': 2, 'assigned_to': 3, 'device': 2}, {'a_id': 3, 'assign_person': 2, 'assigned_to': 3, 'device': 3}]" ),
-                          (3, 3, 3, "[{'a_id': 1, 'assign_person': 2, 'assigned_to': 3, 'device': 1}, {'a_id': 2, 'assign_person': 2, 'assigned_to': 3, 'device': 2}]"),
-                          (2, 2, 3, "[{'a_id': 1, 'assign_person': 2, 'assigned_to': 3, 'device': 1}, {'a_id': 2, 'assign_person': 2, 'assigned_to': 3, 'device': 2}]"),
-                          (2, 3, 8, "[{'a_id': 1, 'assign_person': 2, 'assigned_to': 3, 'device': 1}, {'a_id': 2, 'assign_person': 2, 'assigned_to': 3, 'device': 2}]")])
-def test_new_assignment(assign_person, assigned_to, device, output):
-    dev.make_json_input()
-    assignments = dev.new_assignment(assign_person, assigned_to, device)
-    assert str(assignments) == output
+@pytest.mark.parametrize('fn, ln, gender, role, phone, dob, h, w, output',
+                         [('DD', 'D', 'Female', 'Patient', '2222223333', '1999-10-15', 175, 55, 5),
+                          ('EE', 'E', 'M', 'Doctor', '3333332222', '1998-11-22', 175, 60, None),
+                          ('EE', 'E', 'Male', 'D', '3333332222', '1998-11-22', 175, 60, None)])
+def test_insert_user(fn, ln, gender, role, phone, dob, h, w, output):
+    conn = dm.create_connection(db_dir)
+    last_id = dm.insert_user(conn, fn, ln, gender, role, phone, dob, h, w)
+    conn.close()
+    assert last_id == output
 
-@pytest.mark.parametrize('assignment, value, output',
-                         [(1, 110, "[{'r_id': 1, 'a_id': 1, 'value': 100.0}, {'r_id': 2, 'a_id': 2, 'value': 37.5}, {'r_id': 3, 'a_id': 1, 'value': 110}]"),
-                          (3, 100, "[{'r_id': 1, 'a_id': 1, 'value': 100.0}, {'r_id': 2, 'a_id': 2, 'value': 37.5}]")]
-                         )
-def test_new_data_capture(assignment, value, output):
-    dev.make_json_input()
-    records = dev.new_data_captured(assignment, value)
-    assert str(records) == output
+@pytest.mark.parametrize('id, output',
+                         [('6', 0),
+                          ('5', 0)])
+# output 0 because the auto_increment primary key didn't change in this function
+
+def test_delete_user(id, output):
+    conn = dm.create_connection(db_dir)
+    last_id = dm.delete_user(conn, id)
+    conn.close()
+    assert last_id == output
+
+@pytest.mark.parametrize('id, phone, h, w, output',
+                         [('4', '1112223333', 180, 72, "(4, 'CC', 'C', 'Male', 'Patient', '1112223333', '2000-11-06', 180, 72)"),
+                          ('4', '1111100000', 180, 80, "(4, 'CC', 'C', 'Male', 'Patient', '1111100000', '2000-11-06', 180, 80)"),
+                          ('5', '1111100000', 180, 80, 'None')])
+def test_update_user(id, phone, h, w, output):
+    conn = dm.create_connection(db_dir)
+    user = dm.update_user(conn, id, phone, h, w)
+    conn.close()
+    assert str(user) == output
