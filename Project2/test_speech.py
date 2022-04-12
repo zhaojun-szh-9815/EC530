4545d300ee5df0582868249ded7e58b4014fa8da
朝菌君桾覠頵 <55321300+zhaojun-szh-9815@users.noreply.github.com>
2022-04-11 22:46:40 -0400

    Add files via upload

---
+++ Project2/test_speech.py
@@ -0,0 +1,8 @@
+import pytest
+import speech_api as api
+
+@pytest.mark.parametrize('filename, output',
+                         [('audio.wav', 'your paris sufficient i said')])
+def test_prediction(filename, output):
+    text = api.speech_to_text_task(filename)
+    assert text == output
