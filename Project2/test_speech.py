0921dfbb78da66a5c6f5c96c2a5f077488634c6c
朝菌君桾覠頵 <55321300+zhaojun-szh-9815@users.noreply.github.com>
2022-04-11 22:54:04 -0400

    Update test_speech.py

--- Project2/test_speech.py
+++ Project2/test_speech.py
@@ -2,7 +2,7 @@ import pytest
 import speech_api as api
 
 @pytest.mark.parametrize('filename, output',
-                         [('audio.wav', 'your paris sufficient i said')])
+                         [('client_audio.wav', 'your paris sufficient i said')])
 def test_prediction(filename, output):
     text = api.speech_to_text_task(filename)
     assert text == output
